a2479e55bf64c8bc750ee58d1c175a4274bf2f35
nezl0i <nezl0in@gmail.com>
2022-12-20 12:31:19 +0300

    Отдельный модуль авторизации

--- parser.py
+++ parser.py
@@ -2,26 +2,20 @@ import os
 import sys
 import json
 import pathlib
-import requests
 import pandas as pd
 from lxml import html
 from pathlib import Path
+from authorization import auth
 from dotenv import load_dotenv
 from requests.exceptions import ConnectionError
 
 load_dotenv()
 
-ReturnUrl = None
-RequestVerificationToken = None
 NAME = os.getenv('NAME')
 LOGIN = os.getenv('LOGIN')
 PASSWD = os.getenv('PASSWORD')
 FULL_NAME = None
 
-code = None
-scope = None
-state = None
-session_state = None
 key_teacher = None
 
 Path(pathlib.Path.cwd(), "json", "teachers").mkdir(parents=True, exist_ok=True)
@@ -37,75 +31,24 @@ with open('version', encoding='utf8') as f:
     VERSION = f.read()
 
 base_url = 'https://learn.innopolis.university'
-url = 'https://learn.innopolis.university/Account/Login'
-login_url = 'https://auth.lms.innopolis.university/Account/Login'
-check_url = 'https://learn.innopolis.university/signin-oidc'
 instructors_url = 'https://learn.innopolis.university/Instructors'
 trainings_url = 'https://learn.innopolis.university/api/instructors/trainings'
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-}
-
-params = {'back': ''}
-
-s = requests.Session()
 
 print(f'Версия парсера {VERSION}')
 print('Парсер запущен ...')
 
-# GET запрос базового url для парсинга токена формы
-response = s.get(url, params=params, headers=headers)
+s, response = auth(LOGIN, PASSWD)
 
-# Создание DOM объекта страницы с формой входа
-dom = html.fromstring(response.content)
-user_form = dom.xpath("//div[@class='col-md-9']/form")
+if not response.ok:
+    print('Безуспешная попытка авторизации.')
+    sys.exit()
 
-# Парсим токен из формы
-for item in user_form:
-    ReturnUrl = item.xpath("input[@name='ReturnUrl']/@value")[0]
-    RequestVerificationToken = item.xpath("input[@name='__RequestVerificationToken']/@value")[0]
-
-# Формируем параметры url и данные для POST запроса авторизации
-params = {
-    "ReturnUrl": ReturnUrl
-}
-data = {
-    "Login": LOGIN,
-    "Pass": PASSWD,
-    "button": "login",
-    "__RequestVerificationToken": RequestVerificationToken,
-}
-
-# Авторизуемся
-print('Parsing.. Страница авторизации')
-response = s.post(login_url, params=params, data=data, headers=headers)
-
-# Создаем объект скрытой формы, сформированной в ответе на форму авторизации, для проверки данных.
-# Парсим данные из скрытой формы для проверки и передачи необходимых параметров
-dom_2 = html.fromstring(response.content)
-sign_form = dom_2.xpath('//form')
-
-for item in sign_form:
-    code = item.xpath("input[@name='code']/@value")[0]
-    scope = item.xpath("input[@name='scope']/@value")[0]
-    state = item.xpath("input[@name='state']/@value")[0]
-    session_state = item.xpath("input[@name='session_state']/@value")[0]
-
-check_data = {
-    'code': code,
-    'scope': scope,
-    'state': state,
-    'session_state': session_state
-}
-
-# Проходим проверку
-check_response = s.post(check_url, data=check_data, headers=headers)
-
+print('Parsing.. Авторизация - ОК.')
 print('Parsing.. Страница модулей')
 
 # Парсим страницу "Курсы" для поиска url перехода на следующую страницу
-dom = html.fromstring(check_response.content).xpath(f"{path}/a/@href")[0]
+dom = html.fromstring(response.content).xpath(f"{path}/a/@href")[0]
 
 # Формируем url для перехода на следующую страницу
 online_url = base_url + dom
@@ -270,6 +213,5 @@ for student_dict in data:
 df = pd.DataFrame(values, index=students, columns=columns)
 df.to_excel(os.path.join(f'{xlsx_path}', f'{card_names}.xlsx'),
             sheet_name='events')  # Название файла по наименованию направления
-# df.to_excel(os.path.join(f'{xlsx_path}', f'{FULL_NAME}.xlsx'),
-            # sheet_name='events') # Название файла по преподавателю
+# df.to_excel(os.path.join(f'{xlsx_path}', f'{FULL_NAME}.xlsx'), sheet_name='events') # Название файла по преподавателю
 print('Parsing.. Done.')
